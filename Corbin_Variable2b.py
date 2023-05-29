3297519228243f753d77ab78a25bd9cd7288da7f
theredenemy <1234corbin.1234ad@gmail.com>
2023-05-29 09:50:52 -0700

    modified:   Corbin_Variable2b.py 	Corbin_Print3c.py

---
+++ Corbin_Variable2b.py
@@ -0,0 +1,7 @@
+kilometers = 12.25
+miles = 7.38
+# Old Code did not work
+miles_to_kilometers =  miles * 1.61
+kilometers_to_miles =  kilometers / 1.61
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
