aac7cb48373c7476cf11e38eedf8af38c2e47a8b
theredenemy <1234corbin.1234ad@gmail.com>
2023-06-01 09:31:18 -0700

    modified:   sandbox6.py

--- sandbox6.py
+++ sandbox6.py
@@ -1,4 +1,7 @@
+endofcode = "###############################################################"
 print("I'm Monty Python\"")
 print("I\'m \"Monty Python\"")
+print(endofcode)
 print(2+2)
 print(2 **3)
+
