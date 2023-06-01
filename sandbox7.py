0674c359465afe96ffc8935b2c4e2f9aae0d34db
theredenemy <1234corbin.1234ad@gmail.com>
2023-06-01 10:14:27 -0700

    new file:   sandbox7.py

---
+++ sandbox7.py
@@ -0,0 +1,19 @@
+endofcode = "###############################################################"
+# Sandbox Starts here
+print(endofcode)
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+print(endofcode)
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+print(endofcode)
+print(2 / 3)
+print(2 / 3.)
+print(2. / 3)
+print(2. / 3.)
+print(endofcode)
+
