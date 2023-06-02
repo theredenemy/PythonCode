185166a70288eb5d1b7f56d65a9464d744b634fb
theredenemy <1234corbin.1234ad@gmail.com>
2023-06-02 09:32:09 -0700

    renamed:    Sandbox.py -> SandboxCode/Sandbox.py 	renamed:    sandbox2.py -> SandboxCode/sandbox2.py 	renamed:    sandbox3.py -> SandboxCode/sandbox3.py 	renamed:    sandbox4.py -> SandboxCode/sandbox4.py 	renamed:    sandbox5.py -> SandboxCode/sandbox5.py 	renamed:    sandbox6.py -> SandboxCode/sandbox6.py 	renamed:    sandbox7.py -> SandboxCode/sandbox7.py 	new file:   SandboxCode/sandbox8.py

---
+++ SandboxCode/sandbox8.py
@@ -0,0 +1,11 @@
+endofcode = "###############################################################"
+# Sandbox Starts here
+print(endofcode)
+print(2 // 3)
+print(2 // 3.)
+print(2. // 3)
+print(2. // 3.)
+print(6 // 4)
+print(6. // 4)
+print(-6 // 4)
+print(6. // -4)
