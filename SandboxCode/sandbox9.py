edc828800f9f0f6f03af30a783ac55227ffe89f1
theredenemy <1234corbin.1234ad@gmail.com>
2023-06-05 09:47:28 -0700

    modified:   .gitignore 	new file:   SandboxCode/sandbox9.py

---
+++ SandboxCode/sandbox9.py
@@ -0,0 +1,11 @@
+endofcode = "###############################################################"
+# Sandbox Starts here
+print(endofcode)
+print(-4 + 4)
+print(-4 + 8)
+print(endofcode)
+print(-4 -4)
+print(4. - 8)
+print(-1.1)
+print(+2)
+print(endofcode)
