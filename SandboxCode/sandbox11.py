f2ed6f0b128b44db30b54cd2e84ef2123abcfac0
theredenemy <1234corbin.1234ad@gmail.com>
2023-06-07 09:51:04 -0700

    modified:   SandboxCode/sandbox11.py

---
+++ SandboxCode/sandbox11.py
@@ -0,0 +1,7 @@
+endofcode = "###############################################################"
+# Sandbox Starts here
+print(endofcode)
+print((2 ** 4), (2 * 4.), (2 * 4))
+print(endofcode)
+print((-2 / 4), (2 / 4), (2 // 4),(-2 // 4))
+print(endofcode)
