efe1c08d9675f1528c98e8af083ea291d19eb4e7
theredenemy <1234corbin.1234ad@gmail.com>
2023-06-12 09:35:52 -0700

    modified:   Corbin_Variable2b.py

--- Corbin_Variable2b.py
+++ Corbin_Variable2b.py
@@ -1,7 +1,7 @@
-kilometers = 12.25
-miles = 7.38
+Fahrenheit = 82.1
+Celsius = 1.45
 # Old Code did not work
-miles_to_kilometers =  miles * 1.61
-kilometers_to_miles =  kilometers / 1.61
-print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
-print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+Celsius_to_Fahrenheit =  Celsius * 1.00
+Fahrenheit_to_Celsius =  Fahrenheit / 33.6
+print(Celsius, "Celsius is", round(Celsius_to_Fahrenheit, 2), "Fahrenheit")
+print(Fahrenheit, "Fahrenheit is", round(Fahrenheit_to_Celsius, 2), "Celsius")
