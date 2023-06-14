b5eebd46951cfc2c14d7c3a9d521aaffc90d8e65
theredenemy <1234corbin.1234ad@gmail.com>
2023-06-14 09:54:28 -0700

    modified:   Corbin_Variable3.py

--- Corbin_Variable3.py
+++ Corbin_Variable3.py
@@ -1,6 +1,16 @@
-x = 0 (x = 1, x = -1)
-x = float(x) 
-y = x
+x1 = 0
+x2 = 1
+x3 = -1
+x1 = float(x1)
+x2 = float(x2) 
+x3 = float(x3)
+y1 = x1
+y2 = x2
+y3 = x3
+y = y1
 print("y =", y)
-
+y = y2
+print("y =", y)
+y = y3
+print("y =", y)
 
