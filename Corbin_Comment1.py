c49d24784c5d37c9266c6895ea87c61c89196060
theredenemy <1234corbin.1234ad@gmail.com>
2023-06-15 10:33:41 -0700

    new file:   Corbin_Comment1.py

---
+++ Corbin_Comment1.py
@@ -0,0 +1,12 @@
+# this program computes the number of seconds in each number of hours
+# this program has been written two days ago
+a = 2 # number of hours
+seconds = 3600 # number of seconds in 1 hour
+print("Hours:",a)#printing the number of hours
+# print("Seconds in hours:",a * seconds) # printing number of seconds
+
+
+# here we should also print "Goodbye", but a programmer didn't have time to write any code
+# I Will do it Then - Corbin
+print("Goodbye")
+# this is the end of the program that computes the number of seconds in 3 hour
