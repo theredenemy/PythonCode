0fab12dc1efc051d26dccce326ff98ea709d604f
theredenemy <1234corbin.1234ad@gmail.com>
2023-06-21 10:47:21 -0700

    modified:   SandboxCode/Sandbox16.py

--- SandboxCode/Sandbox16.py
+++ SandboxCode/Sandbox16.py
@@ -1,2 +1,18 @@
-import sandboxlib
-SetupSandbox()
+print("Setting Up Sandbox!")
+endofcodevar = "###############################################################"
+import os
+def SandboxDir():
+    os.system("dir")
+    os.system("tree")
+
+def endofcode():
+    print(endofcodevar)
+
+print("Done!")
+# Sandbox Starts here
+print("+" + 10 * "-" + "+")
+print(("|" + "" * 10 + "|\n") * 5, end="")
+print("+" + 10 * "-" + "+")
+endofcode()
+x = int(input("Enter a number: ")) # The user enters 2
+print(x * "5")
